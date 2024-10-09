6d6f4ec2071753a8bd072bb02e2feab319c01134
testgithubritkaif <muhammadkaifrana1122@gmail.com>
2024-10-09 13:31:07 +0500

    first commint

--- pratice2.py
+++ pratice2.py
@@ -1,20 +1,18 @@
 import time
 hours= int(input ("enter your hours"))
-print(hours)
 timestamp=time.strftime('%H:%M:%S')
-print(timestamp)
-# hours=int(time.strftime('%H'))
-print(hours)
-Mints=time.strftime('%M')
+hours=int(time.strftime('%H'))
+Mint=time.strftime('%M')
 print(Mints)
 Second=time.strftime('%S')
-print(Second)
 if (hours>=0 and hours<12):
     print("good morning")
 elif(hours>=12 and hours<17):
     print("good evening SIR")
-elif(hours>=17 and hours<0):
+elif(hours>=17 and hours<24):
     print("good night sir")
+else:
+    print("invalid input")    
 
 
 
